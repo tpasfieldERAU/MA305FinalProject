cc0023747779204e3a004a544c330a37664277ef
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2022-12-05 15:00:06 -0500

    Add list outputs, general functionality

--- mcpi.py
+++ mcpi.py
@@ -9,49 +9,81 @@ Written By: Thomas Pasfield
 Last Update Date: 12 / 5 / 2022
 -------------------------------------------------------------------------------
 Description:
-    Provides two methods to approximate pi. mcarea() uses the area of a circle 
-    inside a square region, mcvolume() uses the overlap of a sphere and cone in
+    Provides two methods to approximate pi. mc_area() uses the area of a circle 
+    inside a square region, mc_volume() uses the overlap of a sphere and cone in
     a rectangular prism shaped region.
 
-    Each function mentioned above return a double value, which is the last approximation calculated.
+    Each function mentioned above return a double value, which is the last 
+    approximation calculated.
 
-    mcarea_v() returns an array of values reached during execution. Same with mcvolume_v().
+    mc_area_v() returns an array of values reached during execution. 
+    Same with mc_volume_v().
 """
 
 # Imports
 from math import sqrt
 import random
 
+# AREA METHODS
+def area_points():
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    return x,y
+
 def mc_area(N):
     # Random point generation
     points = []
+    count = 0
 
     for i in range(N):
-        points.append([random.uniform(-1,1), random.uniform(-1,1)])
-
-    count_in = 0
-    count_out = 0
-
-    for point in points:
-        a = point[0]
-        b = point[1]
-        if a*a + b*b >= 1.0:
-            count_out += 1
+        x,y = area_points()
+        points.append([x,y])
+        if x*x + y*y >= 1.0:
+            continue
         else:
-            count_in += 1
+            count += 1
+    
+    r = count / N
+    return 4 * r
 
-    r = count_in / N
-    return 4*r
+def mc_area_v(N):
+    # Random point generation
+    points = []
+    vals = []
+    count = 0
 
+    for i in range(N):
+        x,y = area_points()
+        points.append([x,y])
+        if x*x + y*y >= 1.0:
+            continue
+        else:
+            count += 1
+        r = count / (i+1)
+        vals.append(r*4)
+    return vals
+
+# VOLUME METHODS
 def vol_points():
     x = random.uniform(-1,1)
     y = random.uniform(-1,1)
     z = random.uniform( 0,2)
     return x,y,z
 
-def mcvolume_v(N):
+def mc_volume(N):
     points = []
-    plot_vals = []
+    count = 0
+    for i in range(N):
+        x,y,z = vol_points()
+        points.append([x,y,z])
+        if x*x + y*y < z*z and x*x + y*y + (z-1)**2 < 1:
+            count += 1
+    r = count / N
+    return r*8
+
+def mc_volume_v(N):
+    points = []
+    vals = []
     count = 0
     for i in range(N):
         x,y,z = vol_points()
@@ -59,6 +91,6 @@ def mcvolume_v(N):
         if x*x + y*y < z*z and x**2 + y**2 + (z-1)**2 < 1:
             count += 1
         r = count / (i+1) 
-        plot_vals.append(r*8)
+        vals.append(r*8)
 
-    return plot_vals
+    return vals
