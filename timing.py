58a6f1135876d9f5acf70928bfebbb3f3b64d2ee
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2022-12-07 18:44:40 -0500

    Add algorithm timer to compare methods

---
+++ timing.py
@@ -0,0 +1,18 @@
+import mcpi as mc
+import time
+
+print("iter      \tpi      \tEx. Time")
+for i in range(9):
+    iter = 10**i
+    startTime = time.perf_counter()
+    out = mc.mc_area(iter)
+    endTime = time.perf_counter()
+    print(f"{iter:10}\t{out:1.6f}\t{endTime-startTime}")
+
+print("iter      \tpi      \tEx. Time")
+for i in range(9):
+    iter = 10**i
+    startTime = time.perf_counter()
+    out = mc.mc_volume(iter)
+    endTime = time.perf_counter()
+    print(f"{iter:10}\t{out:1.6f}\t{endTime-startTime}")
